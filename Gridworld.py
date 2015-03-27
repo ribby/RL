b3378c28e21598e1e485fd987aece59be09271bf
ribby <alex.rybchuk@gmail.com>
2015-03-26 23:25:54 -0400

    Added special cases A and B to motion. Added reward system. Will need to clean up to return rewards.

--- Gridworld.py
+++ Gridworld.py
@@ -18,26 +18,43 @@ y = np.random.random_integers(0,4)
 print a[x,y]
 
 def update_loc():
+    '''
+    Updates the location on the board and also outputs reward for motion
+    '''
     global x, y
     direction = np.random.random_integers(0,3)
-    if direction == 0:
-        x += 1
-    elif direction == 1:
-        x += -1
-    elif direction == 2:
-        y += 1
-    else:
-        y += -1
-    # Correcting for bounds
-    if x == -1:
-        x = 0
-    if x == 5:
+    reward = 0 # Will use as flag
+    # Special positions A and B
+    if (x,y) == (0,1):
         x = 4
-    if y == -1:
-        y = 0
-    if y == 5:
-        y = 4
-    print a[x,y]
+        reward = 10
+    elif (x,y) == (0,3):
+        x = 2
+        reward = 5
+    # General motion
+    else:
+        if direction == 0:
+            x += 1
+        elif direction == 1:
+            x += -1
+        elif direction == 2:
+            y += 1
+        else:
+            y += -1
+        # Correcting for bounds
+        if x == -1:
+            x = 0
+            reward = -1
+        if x == 5:
+            x = 4
+            reward = -1
+        if y == -1:
+            y = 0
+            reward = -1
+        if y == 5:
+            y = 4
+            reward = -1
+    print a[x,y], reward
 
 update_loc()
 update_loc()
@@ -48,5 +65,30 @@ update_loc()
 update_loc()
 update_loc()
 update_loc()
-
-        
+update_loc()
+update_loc()
+update_loc()
+update_loc()
+update_loc()
+update_loc()
+update_loc()
+update_loc()
+update_loc()
+update_loc()
+update_loc()
+update_loc()
+update_loc()
+update_loc()
+update_loc()
+update_loc()
+update_loc()
+update_loc()
+update_loc()
+update_loc()
+update_loc()
+update_loc()
+update_loc()
+update_loc()
+update_loc()
+update_loc()
+update_loc()
