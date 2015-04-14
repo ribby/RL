aa8750d2ccfbce582c2e1621d4f5036142d22a31
ribby <oromis@live.com>
2015-04-14 10:22:19 -0400

    Restarted QLearn problem. Q is now a dictionary.

--- QLearnCliff.py
+++ QLearnCliff.py
@@ -7,25 +7,28 @@ Created on Sun Apr 12 10:28:09 2015
 
 import numpy as np
 
-# Define grid size
-HEIGHT = 4
-WIDTH = 12
-
-nStates = HEIGHT * WIDTH
-nActions = 4
-
 # Define Q-learning specific constants
 gamma = 1
 epsilon = 0.1
 maxEpisodes = 100
 
-# Initializing the action-value function
-Q = np.zeros([nStates,nActions])
-loc = np.arange(nStates).reshape([HEIGHT,WIDTH])
-print loc
+# Initializing the number of actions. Assuming same number of actions for all state
+nActions = 4
+action = []
+for i in xrange(0,nActions):
+    action.append(0)
+    
+# Define grid size
+HEIGHT = 4
+WIDTH = 12
+nStates = HEIGHT * WIDTH
 
-#==============================================================================
-# for ep in xrange(0,maxEpisodes):
-#     for step in xrange(0,ep:
-#         break    
-#==============================================================================
+# Initializing the action-value function
+grid = np.arange(nStates).reshape([HEIGHT,WIDTH])        # Assigns a number to each state
+Q = {}
+for i in grid:
+    for j in i:
+        Q[j] = action
+print Q
+print state
+
