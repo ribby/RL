6fa89a565f5312a126f25006cadabf1074372880
ribby <oromis@live.com>
2015-04-14 20:56:25 -0400

    Nothing major. Beginning prep on the main function

--- QLearnCliff.py
+++ QLearnCliff.py
@@ -5,15 +5,14 @@ Created on Sun Apr 12 10:28:09 2015
 @author: oromi_000
 
 KNOWN BUGS:
-* In next_action(), epsilon is hardcoded as 100 into the code. This problem 
-probably comes from the global nature of epsilon.
+* epsilon should be revalued
 """
 
 import numpy as np
 
 # Define Q-learning specific constants
 gamma = 1
-epsilon = 100
+epsilon = 100 # Epsilon effectively equals 0.1%,
 maxEpisodes = 100
 alpha = 0.1
 
@@ -36,6 +35,8 @@ for i in grid:
         
 # Define terminal states
 terminal = [37, 38, 39, 40, 41, 42, 43, 44, 45, 46]
+direction = {0: '<', 1: '>', 2: '^', 3: 'v'}
+inv_direction = {v: k for k, v in direction.items()}
 
 def next_states(state):
     '''
@@ -64,21 +65,34 @@ def next_states(state):
     else:
         next_states.append(state+len(grid[0]))
     return next_states
+
     
 def next_action(state):
-    global Q
     global epsilon
     max_action_per_state = []
     next_state = next_states(state)
     for state in next_state:
         max_action_per_state.append(max(Q[state])) # left, right, up, down
     max_action = max(max_action_per_state) 
+    for_direction = max_action_per_state[:] # to be used in else clause for direction
     if np.random.randint(0,100000) > epsilon: # greedy action
-        return max_action
+        return [max_action, direction[max_action_per_state.index(max_action)]]
     else: # exploration
         max_action_per_state.remove(max_action)
         non_optimal_action = max_action_per_state[np.random.randint(0,3)]
-        return non_optimal_action
+        return [non_optimal_action, direction[for_direction.index(non_optimal_action)]]
+
+
+        
+        
+        
+        
+Q[22][1] = 5
+print Q[22][1]
+        
+        
+        
+        
         
 #==============================================================================
 # def update_Q(state,Q,next_state,reward):
@@ -91,16 +105,13 @@ def next_action(state):
 #==============================================================================
     
 
-print Q[14]
-Q[14][0] = 7
-Q[3][2] = 10
-Q[16][3] = 5
-print Q[14]
-test = []
-for i in xrange(10000):
-    test.append(next_action(15))
-
 #==============================================================================
-# for i in xrange(10):
-#     next_action(15)
+# print Q[14]
+# Q[14][0] = 7
+# Q[3][2] = 10
+# Q[16][3] = 5
+# print Q[14]
+# test = []
+# for i in xrange(10000):
+#     test.append(next_action(15))
 #==============================================================================
